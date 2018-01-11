a423f7e8a5f724db9c0261566467754967c90ec2
Jade Kim <hajookim@Hajoos-MacBook-Air.local>
2018-01-10 22:57:03 -0500

    separated files

---
+++ site.js
@@ -0,0 +1,27 @@
+$('.carousel').carousel(); 
+        var captionLength = 0;
+        var caption = '';
+        $(document).ready(function() {
+            setInterval ('cursorAnimation()', 600);
+            captionEl = $('#caption');
+            caption = $('#caption').html();
+            type(); 
+        });
+        
+        function type() {
+
+            captionEl.html(caption.substr(0, captionLength++));
+            if(captionLength < caption.length+1) {
+                setTimeout('type()', 100);
+            } else {
+                captionLength = 0;
+                caption = '';
+            }
+        }
+        function cursorAnimation() {
+            $('#cursor').animate({
+                opacity: 0
+            }, 'fast', 'swing').animate({
+                opacity: 1
+            }, 'fast', 'swing');
+}
