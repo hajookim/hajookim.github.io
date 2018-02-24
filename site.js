3d94f2c17c87614cf27e6c41bf27afe6da9fd790
Jade Kim <hajookim@umich.edu>
2018-02-23 21:02:00 -0500

    new layout?

--- site.js
+++ site.js
@@ -1,29 +1,29 @@
 //starts carousel animation 
-$('.carousel').carousel(); 
-var captionLength = 0;
-var caption = '';
+// $('.carousel').carousel(); 
+// var captionLength = 0;
+// var caption = '';
 
-//typing animation for caption 
-$(document).ready(function() {
-    setInterval ('cursorAnimation()', 600);
-    captionEl = $('#caption');
-    caption = $('#caption').html();
-    type(); 
-});
+// //typing animation for caption 
+// $(document).ready(function() {
+//     setInterval ('cursorAnimation()', 600);
+//     captionEl = $('#caption');
+//     caption = $('#caption').html();
+//     type(); 
+// });
 
-function type() {
-    captionEl.html(caption.substr(0, captionLength++));
-    if(captionLength < caption.length+1) {
-        setTimeout('type()', 100);
-    } else {
-        captionLength = 0;
-        caption = '';
-    }
-}
-function cursorAnimation() {
-    $('#cursor').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
-}
+// function type() {
+//     captionEl.html(caption.substr(0, captionLength++));
+//     if(captionLength < caption.length+1) {
+//         setTimeout('type()', 100);
+//     } else {
+//         captionLength = 0;
+//         caption = '';
+//     }
+// }
+// function cursorAnimation() {
+//     $('#cursor').animate({
+//         opacity: 0
+//     }, 'fast', 'swing').animate({
+//         opacity: 1
+//     }, 'fast', 'swing');
+// }
